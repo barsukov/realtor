45421ef16aa3262300f45047b0d2eb6cc2c8d380
vladimirb <pro.barsukov@gmai.com>
2012-07-31 00:08:30 +0800

    change cms to admin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAdmin::Application.config.secret_token = 'd365b246ea6ce322a7c235d2a47d40ad7ce2365ce87b52b922c6feb74b65721111cbc565a0dcd2c04c3641267e701bb16d9988680e517b79bb96a04f910c5921'
