97251d3fd1fb97b90df087f5e1420c8866d45595
vladimirb <pro.barsukov@gmai.com>
2012-09-04 21:35:19 +0800

    ckeditor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,7 @@ RailsAdmin::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.precompile += Ckeditor.assets
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
